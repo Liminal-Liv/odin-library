555359ccb31249db703204bd4ff07ab5ec9a5fd3
Liv <18509053+frogsforjp@users.noreply.github.com>
2026-04-22 13:16:56 -0400

    feat: Add displayBooks function

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
+function displayBooks() {
+    const display = document.querySelector('.library-display');
+    display.textContent = '';
+
+    for (const book of myLibrary) {
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+
+        // this hidden data id was added to make removing specific books easier
+        card.dataset.id = book.id;
+
+        const title = document.createElement('h2');
+        title.textContent = book.title;
+
+        const author = document.createElement('p');
+        author.textContent = `By ${book.author}`;
+
+        const pages = document.createElement('p');
+        pages.textContent = `${book.pages} pages`;
+
+        const read = document.createElement('p');
+        read.textContent = `${book.read ? "Read" : "Unread"}`
+
+        card.append(title, author, read);
+        display.appendChild(card);
+    }
+}
