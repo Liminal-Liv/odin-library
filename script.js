a7fe2122b9a16bb4e3eba36e09c0b0a83259645a
Liv <18509053+frogsforjp@users.noreply.github.com>
2026-04-22 16:43:27 -0400

    feat: Add new book form and submit logic

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const myLibrary = [];
 
+const newBookBtn = document.querySelector('.new-book');
+const dialog = document.querySelector('.book-dialog');
+const bookForm = document.querySelector('.book-form');
+const cancelBtn = document.querySelector('.cancel-btn');
+
 function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
     this.title = title;
@@ -36,7 +41,30 @@ function displayBooks() {
         const read = document.createElement('p');
         read.textContent = `${book.read ? "Read" : "Unread"}`
 
-        card.append(title, author, read);
+        card.append(title, author, pages, read);
         display.appendChild(card);
     }
 }
+
+function handleSubmitForm(event) {
+    // prevents page from refreshing (default browser behavior)
+    event.preventDefault();
+
+    const title = document.querySelector('.input-title').value;
+    const author = document.querySelector('.input-author').value;
+    const pages = document.querySelector('.input-pages').value;
+    const read = document.querySelector('.input-read').checked;
+
+    addBookToLibrary(title, author, pages, read);
+
+    displayBooks();
+
+    bookForm.reset();
+    dialog.close();
+}
+
+bookForm.addEventListener('submit', handleSubmitForm);
+cancelBtn.addEventListener('click', () => dialog.close());
+newBookBtn.addEventListener(click, ()=> {
+    dialog.showModal();
+});
