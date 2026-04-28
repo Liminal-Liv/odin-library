a87d1ceb322fc729e8a6e2854c008c1366cdd330
Liv <18509053+frogsforjp@users.noreply.github.com>
2026-04-28 18:22:56 -0400

    feat: Add utility button to populate library
    This automatically injects several book objects to the library array to
    help with visual purposes for layout and styling without manual entry.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const newBookBtn = document.querySelector('.new-book');
 const dialog = document.querySelector('.book-dialog');
 const bookForm = document.querySelector('.book-form');
 const cancelBtn = document.querySelector('.cancel-btn');
+const seedLibraryBtn = document.querySelector('.seed-library');
 
 function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
@@ -29,7 +30,7 @@ function displayBooks() {
     for (const book of myLibrary) {
         const card = document.createElement('div');
         card.classList.add('book-card');
-        // this hidden data id was added to make removing specific books easier
+        // this was added for visual purposes so dev tools shows the same id as myLibrary
         card.dataset.id = book.id;
 
         const title = document.createElement('p');
@@ -50,10 +51,7 @@ function displayBooks() {
         const deleteBtn = document.createElement('button');
         deleteBtn.innerText = 'Delete';
         deleteBtn.classList.add('delete-btn');
-        deleteBtn.addEventListener('click', ()=> {
-            console.log('button clicked');
-            removeBook(book.id);
-        });
+        deleteBtn.addEventListener('click', ()=> removeBook(book.id));
 
         card.append(title, author, pages, readStatus, deleteBtn);
         display.appendChild(card);
@@ -87,3 +85,31 @@ cancelBtn.addEventListener('click', () => dialog.close());
 newBookBtn.addEventListener('click', ()=> {
     dialog.showModal();
 });
+
+// for testing purposes and quick library visuals
+function seedLibrary() {
+    const books = [
+        { t: "The Lies of Locke Lamora", a: "Scott Lynch", p: 539, r: true },
+        { t: "Red Seas Under Red Skies", a: "Scott Lynch", p: 576, r: true },
+        { t: "The Republic of Thieves", a: "Scott Lynch", p: 606, r: false },
+        { t: "The Raven Boys", a: "Maggie Stiefvater", p: 409, r: false },
+        { t: "The Dream Thieves", a: "Maggie Stiefvater", p: 439, r: false },
+        { t: "Blue Lily, Lily Blue", a: "Maggie Stiefvater", p: 391, r: false },
+        { t: "The Final Empire", a: "Brandon Sanderson", p: 541, r: true },
+        { t: "The Well of Ascension", a: "Brandon Sanderson", p: 590, r: true },
+        { t: "The Hero of Ages", a: "Brandon Sanderson", p: 572, r: true },
+        { t: "Quiet", a: "Susan Cain", p: 333, r: true },
+        { t: "The Girl with the Dragon Tattoo", a: "Stieg Larsson", p: 465, r: true },
+        { t: "The Girl Who Played with Fire", a: "Stieg Larsson", p: 503, r: true },
+        { t: "The Girl Who Kicked the Hornets' Nest", a: "Stieg Larsson", p: 563, r: true }
+    ];
+
+    books.forEach(b => {
+        const newBook = new Book(b.t, b.a, b.p, b.r);
+        myLibrary.push(newBook);
+    });
+
+    displayBooks();
+}
+
+seedLibraryBtn.addEventListener('click', seedLibrary)
